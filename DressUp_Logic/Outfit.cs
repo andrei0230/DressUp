fe24de0e3f8970152696893ce0cf1c8ca73412a9
Andrei Ciucle <andrei.ciucle02@gmail.com>
2022-11-03 16:02:47 +0200

    Add Outfit class

---
+++ DressUp_Logic/Outfit.cs
@@ -0,0 +1,32 @@
+﻿namespace DressUp_Logic
+{
+    public class Outfit
+    {
+        #region Fields
+
+        private List<Body> bodies;
+        private List<Pants> pants;
+        private List<Shoes> shoes;
+
+        #endregion
+
+        #region Constructors
+
+        public Outfit()
+        {
+            bodies = new List<Body>();
+            pants = new List<Pants>();
+            shoes = new List<Shoes>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}

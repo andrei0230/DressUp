b5f807999a2c797e6346b9fcfc49d3c20faaf409
Andrei Ciucle <andrei.ciucle02@gmail.com>
2022-11-08 19:51:28 +0200

    Add OutfitManager class

---
+++ DressUp_Logic/OutfitManager.cs
@@ -0,0 +1,32 @@
+﻿namespace DressUp_Logic
+{
+    public class OutfitManager
+    {
+        #region Fields
+
+        private List<Body> bodies;
+        private List<Pants> pants;
+        private List<Shoes> shoes;
+
+        #endregion
+
+        #region Constructors
+
+        public OutfitManager()
+        {
+            bodies = new List<Body>();
+            pants = new List<Pants>();
+            shoes = new List<Shoes>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}

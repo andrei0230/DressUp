baa3f1b3546eabdf9a933b7d0cfeceae8231a532
Andrei Ciucle <andrei.ciucle02@gmail.com>
2023-01-06 00:34:00 +0200

    Add overload to GetMatchedOutift method

--- DressUp_Logic/OutfitManager.cs
+++ DressUp_Logic/OutfitManager.cs
@@ -1,12 +1,14 @@
-﻿namespace DressUp_Logic
+﻿using System.IO;
+
+namespace DressUp_Logic
 {
     public class OutfitManager
     {
         #region Fields
 
-        private List<Body> bodies;
-        private List<Pants> pants;
-        private List<Shoes> shoes;
+        private List<Body> _bodylist;
+        private List<Pants> _pantslist;
+        private List<Shoes> _shoeslist;
 
         #endregion
 
@@ -14,43 +16,43 @@
 
         public OutfitManager()
         {
-            bodies = new List<Body>();
-            pants = new List<Pants>();
-            shoes = new List<Shoes>();
+            _bodylist = new List<Body>();
+            _pantslist = new List<Pants>();
+            _shoeslist = new List<Shoes>();
         }
 
         #endregion
 
         #region Properties
 
-        public List<Body> BodyList { get { return bodies; } }
+        public List<Body> BodyList { get { return _bodylist; } }
         
-        public List<Pants> PantsList { get { return pants; } }
+        public List<Pants> PantsList { get { return _pantslist; } }
 
-        public List<Shoes> ShoesList { get { return shoes; } }
+        public List<Shoes> ShoesList { get { return _shoeslist; } }
 
         #endregion
 
         #region Methods
 
-        public void AddItem(Body item) { bodies.Add(item); }
+        public void AddItem(Body item) { _bodylist.Add(item); }
         
-        public void AddItem(Pants item) { pants.Add(item); }
+        public void AddItem(Pants item) { _pantslist.Add(item); }
 
-        public void AddItem(Shoes item) { shoes.Add(item); }
+        public void AddItem(Shoes item) { _shoeslist.Add(item); }
 
-        public void RemoveItem(Body item) { bodies.Remove(item); }
+        public void RemoveItem(Body item) { _bodylist.Remove(item); }
 
-        public void RemoveItem(Pants item) { pants.Remove(item); }
+        public void RemoveItem(Pants item) { _pantslist.Remove(item); }
 
-        public void RemoveItem(Shoes item) { shoes.Remove(item); }
+        public void RemoveItem(Shoes item) { _shoeslist.Remove(item); }
 
         public Outfit GetMatchedOutfit(Body body) 
         {
-            Pants MatchedPants = pants[0];
-            Shoes MatchedShoes = shoes[0];
+            Pants MatchedPants = PantsList[0];
+            Shoes MatchedShoes = ShoesList[0];
 
-            foreach (Pants p in pants)
+            foreach (Pants p in PantsList)
             {
                 if (body.GetColor().Average() == p.GetColor().Average() + 5 || body.GetColor().Average() == p.GetColor().Average() - 5)
                 {
@@ -58,7 +60,7 @@
                 }
             }
 
-            foreach (Shoes s in shoes)
+            foreach (Shoes s in ShoesList)
             {
                 if(body.GetColor().Average() == s.GetColor().Average() + 5 || body.GetColor().Average() == s.GetColor().Average() - 5)
                 {
@@ -67,6 +69,58 @@
             }
 
             Outfit MatchedOutfit = new Outfit(body, MatchedPants, MatchedShoes);
+
+            return MatchedOutfit;
+        }
+
+        public Outfit GetMatchedOutfit(Pants pants)
+        {
+            Body MatchedBody = BodyList[0];
+            Shoes MatchedShoes = ShoesList[0];
+
+            foreach (Body b in BodyList)
+            {
+                if (pants.GetColor().Average() == b.GetColor().Average() + 5 || pants.GetColor().Average() == b.GetColor().Average() - 5)
+                {
+                    MatchedBody = b;
+                }
+            }
+
+            foreach (Shoes s in ShoesList)
+            {
+                if (pants.GetColor().Average() == s.GetColor().Average() + 5 || pants.GetColor().Average() == s.GetColor().Average() - 5)
+                {
+                    MatchedShoes = s;
+                }
+            }
+
+            Outfit MatchedOutfit = new Outfit(MatchedBody, pants, MatchedShoes);
+
+            return MatchedOutfit;
+        }
+
+        public Outfit GetMatchedOutfit(Shoes shoes)
+        {
+            Body MatchedBody = BodyList[0];
+            Pants MatchedPants = PantsList[0];
+
+            foreach (Body b in BodyList)
+            {
+                if (shoes.GetColor().Average() == b.GetColor().Average() + 5 || shoes.GetColor().Average() == b.GetColor().Average() - 5)
+                {
+                    MatchedBody = b;
+                }
+            }
+
+            foreach (Pants p in PantsList)
+            {
+                if (shoes.GetColor().Average() == p.GetColor().Average() + 5 || shoes.GetColor().Average() == p.GetColor().Average() - 5)
+                {
+                    MatchedPants = p;
+                }
+            }
+
+            Outfit MatchedOutfit = new Outfit(MatchedBody, MatchedPants, shoes);
 
             return MatchedOutfit;
         }
